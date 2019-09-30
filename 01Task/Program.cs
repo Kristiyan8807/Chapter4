7867cf2dce274d705f034aa8795f9fce23576055
kristiyan8807 <kristiyan8807@gmail.com>
2019-09-30 23:27:36 +0300

    chapter4

---
+++ 01Task/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишете програма, която чете от конзолата три числа от тип int и
+//отпечатва тяхната сума.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a numbers: ");
+        int a = Int32.Parse(Console.ReadLine());
+        int b = Int32.Parse(Console.ReadLine());
+        int c = Int32.Parse(Console.ReadLine());
+        int result = a + b + c;
+        Console.WriteLine("Result is: " + result);
+
+    }
+}
---
+++ 02Task/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишете програма, която чете от конзолата радиуса "r" на кръг и
+//отпечатва неговото лице и обиколка
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Задайте радиус на кръга: ");
+        int radius = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Лицето на кръга е: " + Math.PI * radius * radius);
+        Console.WriteLine("Обиколката на кръга е: " + 2 * Math.PI * radius);
+
+    }
+}
