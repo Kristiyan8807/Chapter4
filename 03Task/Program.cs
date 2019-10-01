11a5b95810926ab1c5d8f21bf0658fd70d3e2b59
kristiyan8807 <kristiyan8807@gmail.com>
2019-10-01 22:28:23 +0300

    chapter4

---
+++ 03Task/Program.cs
@@ -0,0 +1,32 @@
+﻿//Дадена фирма има име, адрес, телефонен номер, факс номер, уеб сайт
+//и мениджър.Мениджърът има име, фамилия и телефонен номер.
+//Напишете програма, която чете информацията за фирмата и ней-
+//ния мениджър и я отпечатва след това на конзолата.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Моля попълнете данни за фирмата: име, адрес, телефонен номер, факс номер, уеб сайт и мениджър");
+        Console.Write("Име: ");
+        string name = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Адрес: ");
+        string address = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Телефонен номер: ");
+        string phone = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Факс: ");
+        string fax = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Уеб сайт: ");
+        string webSite = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Мениджър: ");
+        string manager = Console.ReadLine();
+        Console.WriteLine();
+
+    }
+}
---
+++ 05Task/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишете програма, която чете от конзолата две цели числа(int) и
+//отпечатва колко числа има между тях, такива, че остатъкът им от де-
+//ленето на 5 да е 0. Пример: в интервала(14, 25) има 3 такива числа:
+//15, 20 и 25.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Напишете две числа: ");
+        int number1 = int.Parse(Console.ReadLine());
+        int number2 = int.Parse(Console.ReadLine());
+        for (int i = number1; i <= number2; i++)
+        {
+            if (i % 5 == 0)
+                Console.WriteLine(i);
+        }
+
+    }
+}
