28fa9a1d18e66ba4fcc272167b6e9cddd7a3cefe
kristiyan8807 <kristiyan8807@gmail.com>
2019-10-03 23:51:30 +0300

    chapter4

--- 07Task/Program.cs
+++ 07Task/Program.cs
@@ -7,13 +7,21 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Напишете пет числа: ");
-        int number1 = int.Parse(Console.ReadLine());
-        int number2 = int.Parse(Console.ReadLine());
-        int number3 = int.Parse(Console.ReadLine());
-        int number4 = int.Parse(Console.ReadLine());
-        int number5 = int.Parse(Console.ReadLine());
-        int sum = number1 + number2 + number3 + number4 + number5;
+        Console.WriteLine("Write five numbers: ");
+        try
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int number4 = int.Parse(Console.ReadLine());
+            int number5 = int.Parse(Console.ReadLine());
+            int sum = number1 + number2 + number3 + number4 + number5;
+            Console.WriteLine(sum);
+        }
+        catch
+        {
+            Console.WriteLine("Error write only numbers");
+        }
         
     }
 
---
+++ 09Task/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишете програма, която прочита едно цяло число n от конзолата.
+//След това прочита още n на брой числа от конзолата и отпечатва
+//тяхната сума.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        Console.Write("Enter numbers count: ");
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < number; i++)
+        {
+            Console.Write("Enter number {0}: ", i + 1);
+            sum += int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("The sum is: " + sum);
+
+    }
+}
