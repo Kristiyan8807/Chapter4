ac2606d0ec20b8aeea592f1a42e5c0f76d6bf330
kristiyan8807 <kristiyan8807@gmail.com>
2019-10-05 21:11:32 +0300

    chapter4

--- 12Task/Program.cs
+++ 12Task/Program.cs
@@ -6,14 +6,8 @@ class Program
 {
     static void Main()
     {
-        //Console.WriteLine(1 / 1);
-        //Console.WriteLine(1.0f / 2);
         Console.Write("Enter a number: ");
         int number = int.Parse(Console.ReadLine());
-        //float sum = 0f;
-        //sum = 1.0f / 1 + 1.0f / 2 + 1.0f / 3 + 1.0f / 4 + 1.0f / 5 + 1.0f / 6 + 1.0f / 7 + 1.0f / 8 + 1.0f / 9 + 1.0f / 10;
-        //Console.WriteLine(sum);
-
         float sum = 0f;
         for (int i = 1; i <= number; i++)
         {
